4d876cd78598ec4e8f884188fd5351703b405927
Ambrose Adeya Ahawo <ahawoadeya995@gmail.com>
2021-11-25 16:45:20 +0300

    update

--- api/models.py
+++ api/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# each model is python class
 class Article(models.Model):
+    # database fields
     title = models.CharField(max_length=100)
     description = models.TextField()
 
-    
+    # view item title on admin list view
     def __str__(self):
         return self.title
--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 from .models import Article
 
-
+# class serializer
 class ArticleSerializer(serializers.Serializer):
+    # specify model types
+    # use serializers instead of models
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=400)
 
-
+    # creating of the article
     def create(self, validated_data):
         return Article.objects.create(validated_data)
 
+    # updating of the article
     def update(self, instance, validated_data):
+        # get instance of the data entries
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('title', instance.description)
+
+
+# class model serializer
+class ArticleModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        # specify the model
+        model   = Article
+        # specify the model fields as a list
+        fields  = ['id', 'title', 'description']
