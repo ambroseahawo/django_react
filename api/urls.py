ed64bd5d87e31e5977d79e4308a55a3da2295af8
Ambrose Adeya Ahawo <ahawoadeya995@gmail.com>
2021-11-26 12:52:32 +0300

    update

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
-from .views import ArticleListViewMixin, ArticleDetailViewMixin
+from django.urls import path, include
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
 
 app_name = "api"
 
+router = DefaultRouter()
+# takes three parameters - prefix, vieset, basename=None
+router.register('articles', ArticleViewSet, basename='articles')
+
 urlpatterns = [
-    path('', ArticleListViewMixin.as_view()),
-    path('<int:id>/', ArticleDetailViewMixin.as_view()),
+    #path('', ArticleListViewMixin.as_view()),
+    #path('<int:id>/', ArticleDetailViewMixin.as_view()),
+    path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -9,6 +9,7 @@ from rest_framework import status
 from rest_framework.decorators import APIView
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import viewsets
 
 # Create your views here.
 
@@ -122,3 +123,37 @@ class ArticleDetailViewMixin(generics.GenericAPIView, mixins.RetrieveModelMixin,
     # delete detail article
     def delete(self, request, id):
         return self.destroy(request, id=id)
+
+
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleModelSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = ArticleModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        #queryset = Article.objects.all()
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleModelSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleModelSerializer(article, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
+        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        
